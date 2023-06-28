193ec156cb1055674fc8161bcfa671c3a8c8f779
Per Steen Olsen <persteen22@hotmail.com>
2023-06-28 18:54:20 +0200

    The Git flow was tested

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).send({
-    message: 'Hello World!',
+    message: 'Hello World!!!',
   });
 });
 
