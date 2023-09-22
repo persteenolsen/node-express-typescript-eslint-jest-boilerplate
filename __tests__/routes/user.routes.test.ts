eb7264f89bf854e19df0ca5fa45fa2352715b3ee
Per Steen Olsen <persteen22@hotmail.com>
2023-09-22 15:58:38 +0200

    The date for Last updated - was updated

--- __tests__/routes/user.routes.test.ts
+++ __tests__/routes/user.routes.test.ts
@@ -5,6 +5,6 @@ import app from "../../src/index";
 describe("Test home route", () => {
   test("Hello world!", async () => {
     const res = await request(app).get("/");
-    expect(res.body).toEqual({ message: "Hello World - Last updated: 02-08-2023!" });
+    expect(res.body).toEqual({ message: "Hello World - Last updated: 22-09-2023!" });
   });
 });
